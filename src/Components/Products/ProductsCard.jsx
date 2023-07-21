9a6f34cd462aa9288f8a615eface9d78c60dce80
Paulo Calderon <paulo.napasa3@gmail.com>
2023-07-20 19:32:07 -0500

    Arreglando

--- src/Components/Products/ProductsCard.jsx
+++ src/Components/Products/ProductsCard.jsx
@@ -3,24 +3,26 @@ import { useParams } from "react-router-dom";
 import useMockData from "../../CustomHook/useMockData";
 import products from '../../mocks/products.json';
 const ProductsCard = () => {
-
+    const { productId } = useParams();
+    // console.log(productId);
     const { data, loading } = useMockData(products)
+    
     if (loading) return <CircularProgress color="secondary" />
     console.log(data);
     return (
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
             
             <Card sx={{ width: "70%", textAlign: "center" }}>
-                <img src={data.images} alt={data.title} style={{ width: "30%", height: "auto" }} />
+                <img src={data[productId].images} alt={data [productId].title} style={{ width: "30%", height: "auto" }} />
                 <CardContent>
                     <Typography variant="h6" component="div">
-                        {data.title}
+                        {data [productId].title}
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                        Price: ${data.price}
+                        Price: ${data[productId].precio}
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                        Description: {data.description}
+                        Descripcion: {data[productId].descripcion}
                     </Typography>
                 </CardContent>
             </Card>
