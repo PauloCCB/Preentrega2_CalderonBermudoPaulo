13b118dac11f44ba53e0d7d75c7dc5cb38d6ce15
Paulo Calderon <paulo.napasa3@gmail.com>
2023-08-16 09:59:27 -0500

    add READ.ME and integration with .env on firebaseConfig

--- src/index.js
+++ src/index.js
@@ -10,30 +10,16 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBY2Giu1wg0XnMEGLJ182gK0oThfh8Ct_g",
-  authDomain: "wearethepoint-597b1.firebaseapp.com",
+  apiKey: process.env.REACT_API_KEY,
+  authDomain: process.env.REACT_AUTH_DOMAIN,
   projectId: "wearethepoint-597b1",
-  storageBucket: "wearethepoint-597b1.appspot.com",
-  messagingSenderId: "525766908773",
-  appId: "1:525766908773:web:00a06bb1c369058099df8c"
+  storageBucket: process.env.REACT_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-
-//iterar sobre los datos del mock para que me suba cada objeto a firestore
-// categories.forEach((producto) => {
-//   // addDoc ,collection,que llamo desde FireStore
-//   // db variable que llama a getFireStore, "productos", que es el nombre de nuestra colección
-//   addDoc(collection(db, 'categories'), producto)
-//     .then((docRef) => {
-//       console.log("Documento agregado con id", docRef.id);
-//     })
-//     .catch((error) => {
-//       console.log("error al agregar el documento", error);
-//     })
-// })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
